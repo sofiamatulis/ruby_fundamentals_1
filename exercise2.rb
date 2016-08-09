ef58f63a18a7fe4cfc6b8fb788295740aa9bdaba
sofiamatulis <sosomatulis@hotmail.com>
2016-08-09 13:43:13 -0400

    added third part of 2nd exercise

--- exercise2.rb
+++ exercise2.rb
@@ -5,3 +5,8 @@ puts 55.00 * 1.13
 #exercise 2 question 2
 
 puts "Maria is " + 21.to_s
+
+
+#exercise 2 question 3
+
+puts "this is the multiplication of two numbers that equals to  #{45628  * 7839 } "
